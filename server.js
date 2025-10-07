1b7c4936ad0eef0bf829a4cd919316291bee0bb7
chewbacca1991 <mounafbanbo@hotmail.com>
2025-10-07 22:13:57 +0330

    Fix MongoDB connection options to prevent deprecation warnings

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB successfully!'))
   .catch(err => console.error('Failed to connect to MongoDB:', err));
 
